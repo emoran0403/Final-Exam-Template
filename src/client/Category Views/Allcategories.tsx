ae4070e286b17cb083c5f5575e327d841b3361a4
Eric Moran <emoran0403@gmail.com>
2022-05-31 16:59:11 -0400

    debugging

--- src/client/Category Views/Allcategories.tsx
+++ src/client/Category Views/Allcategories.tsx
@@ -21,9 +21,6 @@ const Allcategories = (props: Types.CategoriesProps) => {
       });
   }, []);
 
-  console.log(`this is categories array:`);
-  console.log(categoriesArray);
-
   return (
     <div>
       {categoriesArray.map((category) => {
