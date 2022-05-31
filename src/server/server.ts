0695b1fa22526c7959288ae8d01d6ebdb454d8d0
Eric Moran <emoran0403@gmail.com>
2022-05-31 11:12:22 -0400

    added server.ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,1 +1,17 @@
 // this is the start of the server routes - initialize the server here
+
+import * as express from "express";
+import baseRouter from "./Routes/index";
+
+import { configurePassport } from "./server_utils/JWTStrategies";
+
+const app = express();
+
+configurePassport(app);
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(baseRouter);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on ${port}`));
