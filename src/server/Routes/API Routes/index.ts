e2e66f759d6f88a2ccfa5ff6a45fc261faf241e2
Eric Moran <emoran0403@gmail.com>
2022-05-31 11:19:47 -0400

    added baseRouter and apiRouter

--- src/server/Routes/API Routes/index.ts
+++ src/server/Routes/API Routes/index.ts
@@ -1,1 +1,13 @@
 // this will collect all api routes
+
+import * as express from "express";
+import bookRouter from "./BookRoutes";
+import categoryRouter from "./CategoryRoutes";
+
+// current route is /api
+const apiRouter = express.Router();
+
+apiRouter.use(`/books`, bookRouter);
+apiRouter.use(`/categories`, categoryRouter);
+
+export default apiRouter;
--- src/server/Routes/index.ts
+++ src/server/Routes/index.ts
@@ -1,1 +1,12 @@
 // collect all routes here
+
+import * as express from "express";
+import authRouter from "./AUTH Routes";
+import apiRouter from "./API Routes";
+
+const baseRouter = express.Router();
+
+baseRouter.use("/api", apiRouter);
+baseRouter.use("/auth", authRouter);
+
+export default baseRouter;
