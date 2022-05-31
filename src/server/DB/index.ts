7af189072346c854adcb5c0f4e5fe163f32b4ff3
Eric Moran <emoran0403@gmail.com>
2022-05-31 10:42:26 -0400

    wrote db query connection

--- src/server/DB/index.ts
+++ src/server/DB/index.ts
@@ -1,1 +1,20 @@
 //this is where I make the DB connection
+
+import * as mysql from "mysql";
+import * as dotenv from "dotenv";
+import { DB_CONFIG } from "../config";
+
+dotenv.config();
+
+export const Connection = mysql.createPool(DB_CONFIG);
+
+export const Query = <T = mysql.OkPacket>(query: string, values?: unknown[]) => {
+  return new Promise<T>((resolve, reject) => {
+    Connection.query(query, values, (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(result);
+    });
+  });
+};
