eead4414336e49c3ebd1e5b0486ba868efa8d805
Eric Moran <emoran0403@gmail.com>
2022-05-31 14:43:17 -0400

    fixing typing issues

--- src/server/DB/BookQueries.ts
+++ src/server/DB/BookQueries.ts
@@ -7,7 +7,7 @@ const getAllBooks = async () => await Query<Types.SPBook>(`CALL getAllBooks();`)
 
 const createBook = async (newBookInfo: Types.NewBookInfo) => await Query(`INSERT INTO Books SET ?`, [newBookInfo]);
 
-const updateBook = async (id: number, updateBookInfo: Types.NewBookInfo) =>
+const updateBook = async (id: number, updateBookInfo: Types.UpdateBookInfo) =>
   await Query(`UPDATE Books SET ? WHERE id = ?`, [id, updateBookInfo]);
 
 const deleteBook = async (id: number) => await Query<Types.SPBook>(`CALL deleteBook(?);`, [id]);
--- src/server/DB/UserQueries.ts
+++ src/server/DB/UserQueries.ts
@@ -3,7 +3,7 @@
 import { Query } from ".";
 import * as Types from "../../types";
 
-const getSingleUserAuth = async (email: string) => await Query<Types.SPUser>(`CALL getSingleUserAuth(?);`, [email]);
+const getSingleUserAuth = async (email: string) => await Query<Types.SPUser>(`CALL getSingleUserAUTH(?);`, [email]);
 
 const insertNewUser = async (newUserInfo: Types.NewUserInfo) => await Query(`INSERT INTO Users SET ?`, [newUserInfo]);
 
