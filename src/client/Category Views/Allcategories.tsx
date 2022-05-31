63b7efd7ac13ed0e5aff966992d04a2cc47987ff
Eric Moran <emoran0403@gmail.com>
2022-05-31 15:38:57 -0400

    added all categories view

--- src/client/Category Views/Allcategories.tsx
+++ src/client/Category Views/Allcategories.tsx
@@ -1,15 +1,39 @@
 import * as React from "react";
 import * as Types from "../../types";
+import * as Fetcher from "../Client Utils/Fetcher";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Allcategories = (props: Types.CategoriesProps) => {
-  const [state, setState] = useState<string>("");
+  const [categoriesArray, setCategoriesArray] = useState<Types.Category[]>([]);
 
   const nav = useNavigate();
 
-  useEffect(() => {}, []);
-  return <></>;
+  useEffect(() => {
+    Fetcher.GET(`/api/categories`)
+      .then((category) => setCategoriesArray(category))
+      .catch((err) => {
+        console.log(`get all categories error...\n`);
+        console.error(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      {categoriesArray.map((category) => {
+        <div key={category.id}>
+          <div>Title: {category.name}</div>
+          <button
+            onClick={() => {
+              nav(`/categories/${category.id}`, { state: { ...category } });
+            }}
+          >
+            Details
+          </button>
+        </div>;
+      })}
+    </div>
+  );
 };
 
 export default Allcategories;
