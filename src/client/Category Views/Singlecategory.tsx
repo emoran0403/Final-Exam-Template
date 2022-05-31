5f35e341dc3736d25a76211fd97375aa38692c39
Eric Moran <emoran0403@gmail.com>
2022-05-31 15:42:18 -0400

    added single category component

--- src/client/Category Views/Singlecategory.tsx
+++ src/client/Category Views/Singlecategory.tsx
@@ -1,15 +1,28 @@
 import * as React from "react";
 import * as Types from "../../types";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Singlecategory = (props: Types.SingleCategoryProps) => {
-  const [state, setState] = useState<string>("");
-
+  const loc = useLocation();
   const nav = useNavigate();
 
-  useEffect(() => {}, []);
-  return <></>;
+  const CATEGORY = loc.state as Types.Category;
+
+  return (
+    <div>
+      <div>You are viewing the {CATEGORY.name} genre details page</div>
+      <div>
+        {CATEGORY.name} has an ID of {CATEGORY.id} if you wanted to know that for some reason
+      </div>
+      <button
+        onClick={() => {
+          nav(`/categories`);
+        }}
+      >
+        Back to all categories
+      </button>
+    </div>
+  );
 };
 
 export default Singlecategory;
