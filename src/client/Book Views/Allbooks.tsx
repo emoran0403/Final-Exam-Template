a64544e587f3a57d3100427da326ba2beafc4658
Eric Moran <emoran0403@gmail.com>
2022-05-31 15:08:02 -0400

    all books component done

--- src/client/Book Views/Allbooks.tsx
+++ src/client/Book Views/Allbooks.tsx
@@ -1,15 +1,37 @@
 import * as React from "react";
 import * as Types from "../../types";
+import * as Fetcher from "../Client Utils/Fetcher";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Allbooks = (props: Types.BooksProps) => {
-  const [state, setState] = useState<string>("");
+  const [booksArray, setBooksArray] = useState<Types.Book[]>([]);
 
   const nav = useNavigate();
 
-  useEffect(() => {}, []);
-  return <></>;
+  useEffect(() => {
+    Fetcher.GET(`/api/books`)
+      .then((books) => {
+        setBooksArray(books);
+      })
+      .catch((err) => {
+        console.log(`get all books error...\n`);
+        console.error(err);
+      });
+  }, []);
+  return (
+    <div>
+      {booksArray.map((book) => (
+        <div key={book.id}>
+          <div>Title: {book.title}</div>
+          <div>Written by: {book.author}</div>
+          <div>Genre: {book.name}</div>
+          <div>Value: {book.price}</div>
+          <button onClick={() => nav(`/books/${book.id}`, { state: { ...book } })}>Details</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Allbooks;
